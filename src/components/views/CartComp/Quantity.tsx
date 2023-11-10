01afe1a51fad034eeac5256141db0ed7921a7487
Moin Afzal <mmafzal.fcc@gmail.com>
2023-11-10 22:26:28 +0500

    Quantity-Error

--- src/components/views/CartComp/Quantity.tsx
+++ src/components/views/CartComp/Quantity.tsx
@@ -1,38 +1,51 @@
-"use client";
 import { updateCartItems } from "@/components/utils/apiCalling";
 import { singleProductType } from "@/components/utils/types";
 import { typeOfCart } from "@/lib/drizzle";
 import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
 
-export default function Quantity({
+const Quantity = ({
   data,
   user,
-  item
+  item,
 }: {
   data: typeOfCart;
   user: KindeUser;
-  item: singleProductType
-}) {
+  item: singleProductType;
+}) => {
   return (
-    <div className="flex gap-x-2 items-center">
-      <button
-        onClick={() =>
-          updateCartItems(user.id as string, item._id, (data.quantity ? data.quantity : 0 as number) - 1)
-        }
-        className="h-6 w-6 border rounded-full center"
-      >
-        -
-      </button>
-      <span className="text-sm">{data.quantity ? data.quantity : 0}</span>
-      <button
-        onClick={() =>
-          updateCartItems(user.id as string, item._id, (data.quantity ? data.quantity : 0 as number) + 1)
-        }
-        className="h-6 w-6 border rounded-full center"
-      >
-        +
-      </button>
+    <div className="flex justify-between">
+      <div className={` flex gap-2 items-center text-lg`}>
+        <button
+          onClick={() =>
+            updateCartItems(
+              user.id as string,
+              item._id,
+              (data.quantity ? data.quantity : (0 as number)) - 1
+            )
+          }
+          className="select-none cursor-pointer flex justify-center items-center w-8 h-8 rounded-full"
+        >
+          -
+        </button>
+        <p className="ring-sky-200 ring-2 rounded-md cursor-pointer flex justify-center items-center w-8 h-8">
+          Quantity
+        </p>
+        <button
+          onClick={() =>
+            updateCartItems(
+              user.id as string,
+              item._id,
+              (data.quantity ? data.quantity : (0 as number)) + 1
+            )
+          }
+          className="select-none cursor-pointer flex justify-center items-center w-8 h-8 rounded"
+        >
+          +
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default Quantity;
--- src/components/views/CartComp/index.tsx
+++ src/components/views/CartComp/index.tsx
@@ -1,5 +1,106 @@
-"use client";
+// "use client";
 
+// import { handleDelete, productFromIdCart } from "@/components/utils/apiCalling";
+// import {
+//   allProductFetcherFromSanityType,
+//   singleProductType,
+// } from "@/components/utils/types";
+// import { typeOfCart } from "@/lib/drizzle";
+// import { ShoppingBagIcon, X } from "lucide-react";
+// import Link from "next/link";
+// import React, { FC, useEffect, useState } from "react";
+// import Image from "next/image";
+// import { urlForImage } from "../../../../sanity/lib/image";
+// import Quantity from "./Quantity";
+// import Pricing from "./Pricing";
+// import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/server";
+
+// const CartMain: FC<{ data: typeOfCart[]; user: KindeUser }> = ({
+//   data,
+//   user,
+// }) => {
+//   const [productData, setProductData] = useState<singleProductType[]>([]);
+
+//   useEffect(() => {
+//     dataGetter();
+//   }, [data]);
+
+//   if (data.length == 0) {
+//     return (
+//       <div className="h-[80vh] flex items-center justify-center flex-col gap-3">
+//         <ShoppingBagIcon color="purple" size={42} />
+//         <h2 className="text-2xl font-semibold text-gray-600">
+//           Cart is Empty,{" "}
+//           <Link href={"/products"} className="text-xl">
+//             Go to Products Page
+//           </Link>
+//         </h2>
+//       </div>
+//     );
+//   }
+
+//   const dataGetter = async () => {
+//     const productPromise = data.map((item) => {
+//       return productFromIdCart(item.productid);
+//     });
+
+//     try {
+//       const productData = await Promise.all(productPromise);
+
+//       setProductData(
+//         productData.map((item: allProductFetcherFromSanityType) => {
+//           return item.result[0];
+//         })
+//       );
+//     } catch (error) {}
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="max-w-6xl mx-auto">
+//         <div className="font-bold py-8">
+//           <Link href={"/"} className="text-purple-500">
+//             Home
+//           </Link>{" "}
+//           / <span className="text-gray-600">Cart</span>
+//         </div>
+//       </div>
+//       <div className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-16">
+//         <div className="w-[69%]">
+//           {productData.map((item, index: number) => (
+//             <div
+//               key={index}
+//               className="w-full h-[6rem] border-t flex justify-between items-center"
+//             >
+//               <div className="flex items-center gap-8">
+//                 <div className="w-16 h-16">
+//                   <Image
+//                     width={1000}
+//                     height={1000}
+//                     src={urlForImage(item.image[0]).url()}
+//                     alt={item.productname}
+//                   />
+//                 </div>
+//                 <div className="truncate w-80">{item.productname}</div>
+//                 <Quantity user={user} data={data[index]} item={item} />
+//                 <p className="bg-gray-50">${item.price}.00</p>
+//                 <X
+//                   onClick={() => handleDelete(user.id as string, item._id)}
+//                   className="cursor-pointer"
+//                 />
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//         <Pricing data={data} productData={productData} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CartMain;
+
+"use client";
 import { handleDelete, productFromIdCart } from "@/components/utils/apiCalling";
 import {
   allProductFetcherFromSanityType,
@@ -7,9 +108,9 @@ import {
 } from "@/components/utils/types";
 import { typeOfCart } from "@/lib/drizzle";
 import { ShoppingBagIcon, X } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React, { FC, useEffect, useState } from "react";
-import Image from "next/image";
 import { urlForImage } from "../../../../sanity/lib/image";
 import Quantity from "./Quantity";
 import Pricing from "./Pricing";
@@ -20,24 +121,6 @@ const CartMain: FC<{ data: typeOfCart[]; user: KindeUser }> = ({
   user,
 }) => {
   const [productData, setProductData] = useState<singleProductType[]>([]);
-
-  useEffect(() => {
-    dataGetter();
-  }, [data]);
-
-  if (data.length == 0) {
-    return (
-      <div className="h-[80vh] flex items-center justify-center flex-col gap-3">
-        <ShoppingBagIcon color="purple" size={42} />
-        <h2 className="text-2xl font-semibold text-gray-600">
-          Cart is Empty,{" "}
-          <Link href={"/products"} className="text-xl">
-            Go to Products Page
-          </Link>
-        </h2>
-      </div>
-    );
-  }
 
   const dataGetter = async () => {
     const productPromise = data.map((item) => {
@@ -55,6 +138,24 @@ const CartMain: FC<{ data: typeOfCart[]; user: KindeUser }> = ({
     } catch (error) {}
   };
 
+  useEffect(() => {
+    dataGetter();
+  }, [data]);
+
+  if (data.length == 0) {
+    return (
+      <div className="h-[80vh] flex items-center justify-center flex-col gap-3">
+        <ShoppingBagIcon color="purple" size={42} />
+        <h2 className="text-2xl font-semibold text-gray-600">
+          Cart is Empty,{" "}
+          <Link href={"/products"} className="text-xl">
+            Browse products
+          </Link>
+        </h2>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <div className="max-w-6xl mx-auto">
@@ -64,35 +165,35 @@ const CartMain: FC<{ data: typeOfCart[]; user: KindeUser }> = ({
           </Link>{" "}
           / <span className="text-gray-600">Cart</span>
         </div>
-      </div>
-      <div className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-16">
-        <div className="w-[69%]">
-          {productData.map((item, index: number) => (
-            <div
-              key={index}
-              className="w-full h-[6rem] border-t flex justify-between items-center"
-            >
-              <div className="flex items-center gap-8">
-                <div className="w-16 h-16">
-                  <Image
-                    width={1000}
-                    height={1000}
-                    src={urlForImage(item.image[0]).url()}
-                    alt={item.productname}
+        <div className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-16">
+          <div className="w-[69%]">
+            {productData.map((item, index: number) => (
+              <div
+                key={index}
+                className="w-full h-[7rem] border-t flex justify-between items-center"
+              >
+                <div className="flex items-center gap-8">
+                  <div className="w-16 h-16">
+                    <Image
+                      width={50}
+                      height={50}
+                      src={urlForImage(item.image[0]).url()}
+                      alt={item.productname}
+                    />
+                  </div>
+                  <div className="truncate w-80">{item.productname}</div>
+                  <Quantity item={item} user={user} data={data[index]} />
+                  <p className="bg-gray-50">${item.price}.00</p>
+                  <X
+                    onClick={() => handleDelete(user.id as string, item._id)}
+                    className="cursor-pointer"
                   />
                 </div>
-                <div className="truncate w-80">{item.productname}</div>
-                <Quantity user={user} data={data[index]} item={item} />
-                <p className="bg-gray-50">${item.price}.00</p>
-                <X
-                  onClick={() => handleDelete(user.id as string, item._id)}
-                  className="cursor-pointer"
-                />
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <Pricing data={data} productData={productData} />
         </div>
-        <Pricing data={data} productData={productData} />
       </div>
     </div>
   );
